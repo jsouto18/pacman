04c4b38887f5a482948932645fc20bdb84f7ef33
João Souto <jsouto@Joaos-MacBook.lan>
2017-06-26 01:19:43 +0100

    3 working ghosts, follower, ambusher and random ghosts need improvements code wise

--- pacman.js
+++ pacman.js
@@ -10,21 +10,26 @@ var velocityY = 0
 var velocityX = 0
 var positionY = 24
 var positionX = 24
-var ghostVelocityY = [-1,-1,-1,-1] 
+var ghostVelocityY = [0,0,0,0] 
 var ghostVelocityX = [0,0,0,0]
 var ghostPositionY = [312,288,312,288]
 var ghostPositionX = [336,312,312,336]
+var GPt=0
+var GPl=0
 var cntfood = 0
 var cntWall = 0
 var collision = false
 var Ghostcollision = false
 var ghostSurround= [[false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]]
 var renderGhostMovement = true
-var renderGhost = 100
+var renderGhost = 120
 var clientTop= 0
 var clientLeft= 0
+var temppacX=0
+var temppacY = 0
+var flag = 0
 
-var level = "1111111111111111111111111111210000000000001100000000000012101111011111011011110111110121011110111110110111101111101210000000000000000000000000012101111011011111111011011110121011110110111111110110111101210000001100001100001100000012111111011111011011111011111121111110111110110111110111111200000001100000000001100000002111111011011111111011011111121111110000100000010000111111211111101101000000101101111112111111011011100111011011111120000000110000000000110000000211111101111101101111101111112111111011111011011111011111121000000110000110000110000001210111101101111111101101111012101111011011111111011011110121000000000000000000000000001210111101111101101111011111012101111011111011011110111110121000000000000110000000000001211111111111111111111111111112"
+var level = "1111111111111111111111111111210000000000001100000000000012101111011111011011111011110121011110111110110111110111101210000000000000000000000000012101111011011111111011011110121011110110111111110110111101210000001100001100001100000012111111011111011011111011111121111110111110110111110111111200000001100000000001100000002111111011011111111011011111121111110000133333310000111111211111101101333333101101111112111111011011133111011011111120000000110000000000110000000211111101111101101111101111112111111011111011011111011111121000000110000110000110000001210111101101111111101101111012101111011011111111011011110121000000000000000000000000001210111101111101101111101111012101111011111011011111011110121000000000000110000000000001211111111111111111111111111112"
 
 window.addEventListener('load', function(){
 	ghost1.style.top = '312px'
@@ -147,59 +152,50 @@ function ghostPosition(){
 	}
 }
 
-function ghostMovement() {
+function ghostMovement(ha) {
 	setTimeout(function(){
 	ghostPosition()
-	for (var h = 0; h < 4; h++) {
-	var random = Math.floor(Math.random()*3.9999)
-	switch (random) {
-		case 0:
-			if(ghostVelocityX[h]!=1 && ghostSurround[h][0]==false){
-				ghostVelocityX[h] = -1 
-				ghostVelocityY[h] = 0
-			}
-		break;
+	for (var h = ha; h < 3; h++) {
 
-		case 1:
-			if(ghostVelocityY[h]!=1 && ghostSurround[h][1]==false){
-				ghostVelocityX[h] = 0
-				ghostVelocityY[h] = -1
+		if(h == 0 || h == 1){
+			if(h == 1){
+			temppacX = positionX
+			temppacY = positionY
+			positionY = positionY - velocityY*48
+			positionX = positionX - velocityX*48
 			}
-		break;
 
-		case 2:
-			if(ghostVelocityX[h]!=-1 && ghostSurround[h][2]==false){
-				ghostVelocityX[h] = 1
-				ghostVelocityY[h] = 0
-			}
-		break;
+		GPl = ghostPositionX[h] - positionX
+		GPt = ghostPositionY[h] - positionY
+		GPl_ABS = Math.abs(GPl)
+		GPt_ABS = Math.abs(GPt)
 
-
-		case 3:
-			if(ghostVelocityY[h]!=-1 && ghostSurround[h][3]==false){
-				ghostVelocityX[h] = 0
-				ghostVelocityY[h] = 1
-			}
-		break;
+		ghostFollow(h,GPt_ABS,GPl_ABS,GPt,GPl)
 	}
+	
+		if(h==2)
+			ghostrandom(h)
 
-		if(ghostSurround[h][0]==true && ghostVelocityX[h]==-1){
-		ghostVelocityY[h]=-1
-		ghostVelocityX[h]=0
+		if( (ghostPositionX[h] == 336 || ghostPositionX[h] == 312 ) && (ghostPositionY[h] == 312 || ghostPositionY[h] == 288 || ghostPositionY[h] == 336) ){
+			ghostVelocityY[h] = 1
+			ghostVelocityX[h] = 0
 		}
-		if(ghostSurround[h][1]==true && ghostVelocityY[h]==-1){
-			ghostVelocityX[h]=1
-			ghostVelocityY[h]=0
+	
+		if( (ghostPositionX[h] == 336 || ghostPositionX[h] == 312 ) && ghostPositionY[h] == 360 && ghostVelocityY[h] == -1){
+			ghostVelocityY[h] = 0
+			if(GPl > 0){
+					ghostVelocityX[h] = -1
+				} else {
+					ghostVelocityX[h] = 1
+				}
 		}
-		if(ghostSurround[h][2]==true && ghostVelocityX[h]==1){
-			ghostVelocityY[h]=1
-			ghostVelocityX[h]=0
+		if(ghostVelocityX[h] == -1 && ghostSurround[h][0] || ghostVelocityY[h] == -1 && ghostSurround[h][1] || ghostVelocityX[h] == 1 && ghostSurround[h][2] || ghostVelocityY[h] == 1 && ghostSurround[h][3]){
+			renderGhost = 1
+			ghostMovement(h)
+			return
+		}else {
+			renderGhost = 120
 		}
-		if(ghostSurround[h][3]==true && ghostVelocityY[h]==1){
-			ghostVelocityX[h]=0
-			ghostVelocityY[h]=0
-		}
-
 		ghostPositionY[h] = ghostPositionY[h] + ghostVelocityY[h]*24
 		ghostPositionX[h] = ghostPositionX[h] + ghostVelocityX[h]*24
 
@@ -208,12 +204,22 @@ function ghostMovement() {
 		} else if ((ghostPositionY[h] == 36*10 || ghostPositionY[h] == 24*10) && ghostPositionX[h] >27*24) {
 			ghostPositionX[h]=0
 		} 
+		
 		var ghost = 'ghost'+(h+1)
 		document.getElementById(ghost).style.top = ghostPositionY[h]+'px'
 		document.getElementById(ghost).style.left = ghostPositionX[h] +'px'
+
+		
+		if(h==1){
+			positionX = temppacX 
+			positionY = temppacY 
+		}
+
 	}
+
 	ghostSurround = [[false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]]
-	ghostMovement()
+	ghostMovement(0)
+	
 },renderGhost)
 }
 
@@ -250,15 +256,245 @@ function renderWalls() {
 				gameContainer.style.width = wallLeft+'px'
 				wallLeft = 0
 				break;
+
+			case '3':
+				wallLeft += 24
+				break
 		}	
 	}
 	gameContainer.style.height = wallTop+'px'
 }
 
+function ghostrandom(h){
+	var random = Math.floor(Math.random()*3.9999)
+	switch (random) {
+		case 0:
+			if(ghostVelocityX[h]!=1 && !ghostSurround[h][0]){
+				ghostVelocityX[h] = -1 
+				ghostVelocityY[h] = 0
+			}
+		break;
+		case 1:
+			if(ghostVelocityY[h]!=1 && !ghostSurround[h][1]){
+				ghostVelocityX[h] = 0
+				ghostVelocityY[h] = -1
+			}
+		break;
+		case 2:
+			if(ghostVelocityX[h]!=-1 && !ghostSurround[h][2]){
+				ghostVelocityX[h] = 1
+				ghostVelocityY[h] = 0
+			}
+		break;
+		case 3:
+			if(ghostVelocityY[h]!=-1 && !ghostSurround[h][3]){
+				ghostVelocityX[h] = 0
+				ghostVelocityY[h] = 1
+			}
+		break;
+	}
+	console.log(random)
+}
+
+function ghostFollow(h,GPt_ABS,GPl_ABS,GPt,GPl){
+if(!ghostSurround[h][0] && !ghostSurround[h][1] && !ghostSurround[h][2] && !ghostSurround[h][3]){
+			if(GPt_ABS >= GPl_ABS && ghostVelocityY[h] == 0){
+				if(GPt >= 0){
+					ghostVelocityY[h] = -1
+					ghostVelocityX[h] = 0
+				} else {
+					ghostVelocityY[h] = 1
+					ghostVelocityX[h] = 0
+				}
+			} else if(GPt < 0 && ghostVelocityX[h] == 0){
+				if(GPl >= 0){
+					ghostVelocityX[h] = -1
+					ghostVelocityY[h] = 0
+				} else{
+					ghostVelocityX[h] = 1
+					ghostVelocityY[h] = 0
+				}
+			}
+		} else if(!ghostSurround[h][3] && !ghostSurround[h][0] && !ghostSurround[h][1]){
+			if(GPt_ABS >= GPl_ABS || ghostVelocityX[h] == 1){
+				if(GPt > 0 && ghostVelocityY[h] != 1){
+					ghostVelocityY[h] = -1
+					ghostVelocityX[h] = 0
+				} else if(ghostVelocityY[h] != -1){
+					ghostVelocityY[h] = 1
+					ghostVelocityX[h] = 0
+				}
+			} else{
+				if(GPl > 0){
+					ghostVelocityX[h] = -1
+					ghostVelocityY[h] = 0
+				} else {
+					if(GPt > 0){
+						ghostVelocityY[h] = -1
+						ghostVelocityX[h] = 0
+					} else {
+						ghostVelocityY[h] = 1
+						ghostVelocityX[h] = 0
+					}
+				}
+			}
+		} else if(!ghostSurround[h][0] && !ghostSurround[h][1] && !ghostSurround[h][2]){
+			if(GPt_ABS >= GPl_ABS && ghostVelocityY[h] != 1){
+				if(GPt > 0){
+					ghostVelocityY[h] = -1
+					ghostVelocityX[h] = 0
+				} else {
+					if(GPl >= 0){
+						if(ghostVelocityX[h] == 1){
+							ghostVelocityX[h] = 1
+						}else{
+						ghostVelocityX[h] = 1
+						ghostVelocityY[h] = 0
+						}
+					} else {
+						if(ghostVelocityX[h] == -1){
+							ghostVelocityX[h] = -1
+						} else {
+						ghostVelocityX[h] = 1
+						ghostVelocityY[h] = 0
+						}
+					}
+				}
+			} else {
+				if(GPl > 0){
+					if(ghostVelocityX[h] == 1){
+						ghostVelocityY[h] = -1
+						ghostVelocityX[h] = 0
+					} else {
+					ghostVelocityX[h] = -1
+					ghostVelocityY[h] = 0
+					}
+				} else if(GPl < 0 && ghostVelocityX[h] != -1){
+					ghostVelocityX[h] = 1
+					ghostVelocityY[h] = 0
+				}
+			}
+		} else if(!ghostSurround[h][1] && !ghostSurround[h][2] && !ghostSurround[h][3]){
+			if(GPt_ABS >= GPl_ABS){
+				if(GPt > 0){
+					if(ghostVelocityY[h] == 1){
+						ghostVelocityY[h] = 0
+						ghostVelocityX[h] = 1
+					} else {
+					ghostVelocityY[h] = -1
+					ghostVelocityX[h] = 0
+					}
+				} else {
+					if(ghostVelocityY[h] == -1){
+						ghostVelocityY[h] = 0
+						ghostVelocityX[h] = 1
+					} else {
+					ghostVelocityY[h] = 1
+					ghostVelocityX[h] = 0
+					}
+				}
+			} else {
+				if(GPl > 0 || ghostVelocityX[h] == -1){
+					if(GPt > 0){
+						ghostVelocityY[h] = -1
+						ghostVelocityX[h] = 0
+					} else {
+						ghostVelocityY[h] = 1
+						ghostVelocityX[h] = 0
+					}
+				} else {
+					ghostVelocityX[h] = 1
+					ghostVelocityY[h] = 0
+				}
+			}
+		} else if(!ghostSurround[h][2] && !ghostSurround[h][3] && !ghostSurround[h][0]){
+			if(GPt_ABS >= GPl_ABS && ghostVelocityY[h] != -1){
+				if(GPt > 0){
+					if(GPl > 0){
+						ghostVelocityX[h] = -1
+						ghostVelocityY[h] = 0
+					} else {
+						ghostVelocityX[h] = 1
+						ghostVelocityY[h] = 0
+					}
+				} else {
+					ghostVelocityY[h] = 1
+					ghostVelocityX[h] = 0
+				}
+			} else {
+				if(GPl > 0){
+					if(ghostVelocityX[h] == 1){
+						ghostVelocityX[h] = 0
+						ghostVelocityY[h] =1
+					} else {
+						ghostVelocityX[h] = -1
+						ghostVelocityY[h] = 0
+					}	
+				} else {
+					if(ghostVelocityX[h] == -1){
+						ghostVelocityX[h] = 0
+						ghostVelocityY[h] = 1
+					} else {
+						ghostVelocityX[h] = 1
+						ghostVelocityY[h] = 0
+					}
+				}
+			}
+		} else if(!ghostSurround[h][0] && !ghostSurround[h][1]){
+			if( ghostVelocityX[h] == 1 ){
+				ghostVelocityY[h] = -1
+				ghostVelocityX[h] = 0
+			} else if( ghostVelocityY[h] == 1 ){
+				ghostVelocityY[h] = 0
+				ghostVelocityX[h] = -1
+			}
+		} else if(!ghostSurround[h][1] && !ghostSurround[h][2]){
+			if( ghostVelocityX[h] == -1 ){
+				ghostVelocityY[h] = -1
+				ghostVelocityX[h] = 0
+			} else if( ghostVelocityY[h] == 1 ){
+				ghostVelocityY[h] = 0
+				ghostVelocityX[h] = 1
+			}
+		} else if(!ghostSurround[h][2] && !ghostSurround[h][3]){
+			if( ghostVelocityX[h] == -1 ){
+				ghostVelocityY[h] = 1
+				ghostVelocityX[h] = 0
+			} else if( ghostVelocityY[h] == -1 ){
+				ghostVelocityY[h] = 0
+				ghostVelocityX[h] = 1
+			}
+		} else if(!ghostSurround[h][3] && !ghostSurround[h][0]){
+			if( ghostVelocityX[h] == 1 ){
+				ghostVelocityY[h] = 1
+				ghostVelocityX[h] = 0
+			} else if( ghostVelocityY[h] == -1 ){
+				ghostVelocityY[h] = 0
+				ghostVelocityX[h] = -1
+			}
+		} else if(!ghostSurround[h][0] && !ghostSurround[h][2]){
+			if(GPl > 0 && ghostVelocityX[h] != 1){
+				ghostVelocityX[h] = -1
+				ghostVelocityY[h] = 0
+			} else if(GPl <= 0 && ghostVelocityX[h] != -1) {
+				ghostVelocityX[h] = 1
+				ghostVelocityY[h] = 0
+			}
+		} else if(!ghostSurround[h][1] && !ghostSurround[h][3]){
+				if(GPt > 0 && ghostVelocityY[h] != 1){
+					ghostVelocityY[h] = -1
+					ghostVelocityX[h] = 0
+				} else if(GPt <= 0 && ghostVelocityY[h] != -1){
+					ghostVelocityY[h] = 1
+					ghostVelocityX[h] = 0
+				}
+			}
+}
+
 start.addEventListener("click", function(){
 	start.style.display = 'none'
 	move()
-	ghostMovement()
+	ghostMovement(0)
 })
 
 
